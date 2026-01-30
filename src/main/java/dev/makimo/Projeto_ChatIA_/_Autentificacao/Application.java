89165f0faf005eba30f31205430a6d201cf3fe72
makimo-to <israelcsantos515@gmail.com>
2026-01-30 02:39:40 -0300

    primeiro commit do projeto

---
+++ src/main/java/dev/makimo/Projeto_ChatIA_/_Autentificacao/Application.java
@@ -0,0 +1,13 @@
+package dev.makimo.Projeto_ChatIA_._Autentificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/makimo/Projeto_ChatIA_/_Autentificacao/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.makimo.Projeto_ChatIA_._Autentificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
